2634d2dfaefb7b5459f9f0047b5022e2fc1a90c4
Ingrid Carlson <ingridclaire@gmail.com>
2024-02-06 18:21:36 -0500

    sets up component files, creates basic structure for components

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.jsx"],
+      rules: {
+        "react/prop-types": "off", // Turning off prop-types check for all .js and .jsx files
+      },
+    },
+  ],
+};
---
+++ src/components/BackgroundHeading.jsx
@@ -0,0 +1,3 @@
+export default function BackgroundHeader() {
+  return <h1>Trekbag</h1>;
+}
