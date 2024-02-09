701a2381549c80b9573a1476a16c73071125aeb0
Ingrid Carlson <ingridclaire@gmail.com>
2024-02-09 11:35:15 -0500

    adds state to item list, creates lib folder and constants file

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import { initialItems } from "../lib/constants";
+
 export default function ItemList() {
-  return <div>ItemList</div>;
+  const [items, setItems] = useState(initialItems);
+  return (
+    <ul>
+      {items.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" checked={item.packed} />
+        {item.name}
+      </label>
+      <button>❌</button>
+    </li>
+  );
 }
